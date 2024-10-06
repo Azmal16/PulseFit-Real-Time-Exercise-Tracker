1e3ca5af6bd29e552a50d28642381120dbfd1c1a
Azmal Awasaf <azmalawasaf@gmail.com>
2024-10-07 05:59:35 +0600

    Initial Commit

---
+++ PulseFit/PulseFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseFitApp.swift
+//  PulseFit
+//
+//  Created by Azmal Awsaf on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PulseFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PulseFitTests/PulseFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  PulseFitTests.swift
+//  PulseFitTests
+//
+//  Created by Azmal Awsaf on 7/10/24.
+//
+
+import Testing
+@testable import PulseFit
+
+struct PulseFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
