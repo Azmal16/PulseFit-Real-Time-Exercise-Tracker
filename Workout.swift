dfe36de1397ce99286910afb6765c906e40765f4
Azmal Awasaf <azmalawasaf@gmail.com>
2024-10-07 08:39:59 +0600

    Add files via upload

---
+++ Workout.swift
@@ -0,0 +1,13 @@
+//
+//  Workout.swift
+//  PulseFit
+//
+//  Created by Azmal Awsaf on 7/10/24.
+//
+
+
+struct Workout {
+    var name: String
+    var count: Int = 0
+    var isComplete: Bool = false
+}
