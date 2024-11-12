0438b15009f592ba50ca7743b09862b5f6061cdc
Azmal Awasaf <aawsaf@mymedicalhub.com>
2024-11-12 21:01:14 +0600

    unwrap video output

--- PulseFit/CameraManager.swift
+++ PulseFit/CameraManager.swift
@@ -14,34 +14,36 @@ class CameraManager: NSObject, ObservableObject {
             print("Failed to access the camera")
             return
         }
-
+        
         if session?.canAddInput(input) ?? false {
             session?.addInput(input)
         }
-
+        
         // Setup video output
         videoOutput = AVCaptureVideoDataOutput()
         videoOutput?.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
         
-        if session?.canAddOutput(videoOutput!) ?? false {
-            session?.addOutput(videoOutput!)
+        guard let videoOutput = videoOutput else { return }
+        
+        if session?.canAddOutput(videoOutput) ?? false {
+            session?.addOutput(videoOutput)
         }
     }
-
+    
     // Start the camera session
     func startSession() {
         DispatchQueue.global(qos: .background).async {
             self.session?.startRunning()
         }
     }
-
+    
     // Stop the camera session
     func stopSession() {
         DispatchQueue.global(qos: .background).async {
             self.session?.stopRunning()
         }
     }
-
+    
     func getSession() -> AVCaptureSession? {
         return session
     }
